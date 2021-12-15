8adcb706c7737aa67836559facb8e4a435ff9c94
Jess Izumi <izumijessica@gmail.com>
2021-12-15 18:11:50 -0500

    html5up template with routing for navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlanesComponent } from './planes/planes.component';
+import { TrainsComponent } from './trains/trains.component';
+import { CarsComponent } from './cars/cars.component';
+import { BikesComponent } from './bikes/bikes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'planes', component: PlanesComponent },
+  { path: 'trains', component: TrainsComponent },
+  { path: 'cars', component: CarsComponent },
+  { path: 'bikes', component: BikesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlanesComponent } from './planes/planes.component';
+import { TrainsComponent } from './trains/trains.component';
+import { CarsComponent } from './cars/cars.component';
+import { BikesComponent } from './bikes/bikes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlanesComponent,
+    TrainsComponent,
+    CarsComponent,
+    BikesComponent
   ],
   imports: [
     BrowserModule,
